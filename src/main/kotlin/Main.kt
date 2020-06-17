2035f4d3717d62f05d2fc0874f81a982e551ac2b
Chinseok Lee <me@askcompany.kr>
2020-06-17 21:06:10 +0900

    Komoran객체에 userDict 지정

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,21 @@
+import com.google.gson.Gson
+import kr.co.shineware.nlp.komoran.constant.DEFAULT_MODEL
+import kr.co.shineware.nlp.komoran.core.Komoran
 import org.apache.log4j.BasicConfigurator
-import spark.Spark.*;
+import spark.Spark.post
 
 fun main(args: Array<String>) {
     BasicConfigurator.configure();
 
-    get("/hello") { req, res ->
-        "Hello World"
-    }
+    val komoran = Komoran(DEFAULT_MODEL.LIGHT)
+
+    val userDir = System.getProperty("user.dir")
+     komoran.setUserDic("$userDir/userdic.txt")
+
+    post("/summarize", { request, response ->
+        response.type("application/json")
+        val sentenceList = Gson().fromJson(request.body(), Array<String>::class.java)
+        val data = arrayOf("hello", "world", "python", "django")
+        data
+    }, Gson()::toJson)
 }
