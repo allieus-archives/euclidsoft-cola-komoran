cabcab61d5d3b6c5449a11a8b308f5070be63d55
Chinseok Lee <me@askcompany.kr>
2020-06-17 23:16:01 +0900

    Komoran을 통한 tokenize API 구현. 메모리 이슈로 userdic.txt 로딩은 주석

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,27 @@
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import kr.co.shineware.nlp.komoran.constant.DEFAULT_MODEL
 import kr.co.shineware.nlp.komoran.core.Komoran
 import org.apache.log4j.BasicConfigurator
 import spark.Spark.post
+import java.lang.reflect.Type
+
 
 fun main(args: Array<String>) {
     BasicConfigurator.configure();
 
     val komoran = Komoran(DEFAULT_MODEL.LIGHT)
 
-    val userDir = System.getProperty("user.dir")
-    komoran.setUserDic("$userDir/userdic.txt")
+    // FIXME: 메모리 이슈로 임시 주석 처리
+    // val userDir = System.getProperty("user.dir")
+    // komoran.setUserDic("$userDir/userdic.txt")
 
-    post("/summarize", { request, response ->
+    val type: Type = object : TypeToken<Map<String?, String?>?>() {}.type
+
+    post("/tokenize", { request, response ->
         response.type("application/json")
-        val sentenceList = Gson().fromJson(request.body(), Array<String>::class.java)
-        val data = arrayOf("hello", "world", "python", "django")
-        data
+        val postMap: Map<String, String> = Gson().fromJson(request.body(), type)
+        val sentence = postMap["sentence"] ?: ""
+        komoran.analyze(sentence).nouns
     }, Gson()::toJson)
 }
