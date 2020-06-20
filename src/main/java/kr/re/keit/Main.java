e3627b703cb8855e800dfec436f5333e770f067a
Chinseok Lee <me@askcompany.kr>
2020-06-20 11:53:15 +0900

    서버 시작 로그메세지 추가

--- src/main/java/kr/re/keit/Main.java
+++ src/main/java/kr/re/keit/Main.java
@@ -9,10 +9,12 @@ public class Main {
     static final int port = 50051;
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Initializing ...");
         Server server = ServerBuilder
             .forPort(port)
             .addService(new KomoranService())
             .build();
+        System.out.println("Server started ...");
         server.start().awaitTermination();
     }
 }
